1395599fe290afcdf84bf243ed1de938d9e17f2c
dci <priya.shah174@gmail.com>
2021-08-31 14:19:08 +0200

    established connection to mongoDB in sever.js

---
+++ server.js
@@ -0,0 +1,14 @@
+//connection to mongoDB using mongoose
+
+import mongoose from "mongoose";
+
+const MONGO_URI =
+  "mongodb+srv://new_use_1:7qQHu6lDWhWj3Me9@cluster0.ft5sz.mongodb.net/students_db?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection successful!"))
+  .catch((err) => console.log("Connection sucks!!"), err);
