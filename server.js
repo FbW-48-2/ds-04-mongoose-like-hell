8438158b50a1c426d13f36be10371706c0f7c2f0
dci <priya.shah174@gmail.com>
2021-08-31 15:09:12 +0200

    added route to get all students already with try, catch & created central error handler. Testing for the following route done and working

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 import express from "express";
-
-//initializing middleware - express here
-
-const app = express();
+import Student from "./models/Student.js";
 
 //connection to mongoDB using mongoose
 
@@ -16,4 +13,39 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connection successful!"))
-  .catch((err) => console.log("Connection sucks!!"), err);
+  .catch((err) => console.log("Connection sucks!!", err));
+
+//initializing middleware - express here
+
+const app = express();
+app.use(express.json());
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`ÀPI started up on http://localhost:${PORT}`);
+});
+
+//making my CRUD operations below
+
+app.get("/", (req, res) => {
+  res.send("Hi! This is the students API");
+});
+
+//get all students below
+
+app.get("/students", async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//get single student by id
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(400).json({
+    error: err.message,
+  });
+});
