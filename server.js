5078580009f0772f1b91974d878d84d1b8fc1d22
Jonathan Shine <jonathanhshine@gmail.com>
2021-08-31 15:41:59 +0200

    TASK_2 completed: outsourced MongoDB connection to db-connect.js, created all CRUD routes implementing Mongoose methods, added error handling

--- server.js
+++ server.js
@@ -1,19 +1,97 @@
-import mongoose from 'mongoose';
-const { connect } = mongoose;
+// EXPRESS ---------------------------------------------
+import express from 'express';
+const app = express();
+import './db-connect.js';
+import Student from './models/Student.js';
+
+    // MIDDLEWARE --------------------------------------
+    app.use( express.json() );
 
 
-// DATABASE CONNECTION ---------------------------------
-const MONGO_URI = "mongodb+srv://Jon19:Jon19@db-intro.odc0h.mongodb.net/students_db?retryWrites=true&w=majority";
+    // ROUTES ------------------------------------------
+        // HOME
+        app.get("/", (req, res) => {
+            res.send(`
+            <h1>Welcome to the students API!</h1>
+            <a href="/students">/students</a>
+            `)
+        });
 
-connect( MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
-.then(() => {
-    console.log("CONNECTION SUCCESSFUL");
-})
-.catch((err) => {
-    console.log("CONNECTION FAILED -->", err.message);
-})
+        // GET all students
+        app.get("/students", async (req, res, next) => {
+            try {
+                const students = await Student.find();
+                res.json( students );
+            } catch (error) {
+                next( error );
+            };
+        });
+
+        // GET single student
+        app.get("/students/:id", async (req, res, next) => {
+            const { id } = req.params;
+            console.log( id );
+            try {
+                const student = await Student.findById( id );
+                res.json( student );
+            } catch (error) {
+                next( error );
+            };
+        });
+
+        // POST create a student
+        app.post("/students", async (req, res, next) => {
+            const body = req.body;
+            console.log( body );
+            try {
+                const studentNew = await Student.create( body );
+                res.json( studentNew );
+            } catch (error) {
+                next( error );
+            };
+        });
+
+        // PUT update student using id
+        app.put("/students/:id", async (req, res, next) => {
+            const { id } = req.params;
+            const studentData = req.body;
+            console.log( id, studentData );
+            try {
+                const studentUpdated = await Student.findByIdAndUpdate(
+                    id,
+                    studentData,
+                    { new: true }
+                );
+                res.json( studentUpdated );
+            } catch (error) {
+                next( error );
+            }
+        });
+
+        // DELETE student by id
+        app.delete("/students/:id", async (req, res, next) => {
+            const { id } = req.params;
+            console.log( id );
+            try {
+                const studentDeleted = await Student.findByIdAndDelete( id );
+                res.json( studentDeleted );
+            } catch (error) {
+                next( error );
+            }
+        });
+
+    
+    // ERROR HANDLER -----------------------------------
+    app.use((err, req, res, next) => {
+        console.log( err );
+        res.status(400).json({
+            error: err.message
+        });
+    });
+
+    // ACTIVATE PORT -----------------------------------
+    const PORT = 5000;
+    app.listen( PORT, () => {
+        console.log(`API is up and running on http://localhost:${PORT}`);
+    })
 // -----------------------------------------------------
