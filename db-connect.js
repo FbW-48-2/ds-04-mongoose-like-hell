62143c37fe640140e720a1e725efffb0e7e2896a
Shinhee Chae <shinheechae@gmail.com>
2021-09-01 09:03:05 +0200

    some info hidden

--- db-connect.js
+++ db-connect.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
  
 
 //const Mongo_URL = "mongodb://localhost/users_db";
-const Mongo_URL = "mongodb+srv://shinhee:melatonin@shinhee.fglsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const Mongo_URL = "mongodb+srv://shinhee:*******@shinhee.fglsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(Mongo_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=> console.log("up & running, Shinhee!"))
