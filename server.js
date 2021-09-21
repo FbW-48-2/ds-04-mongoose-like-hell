b7a018dd686780337822f2cd768acb1b5f9a718c
Priya <58366904+lifewave174@users.noreply.github.com>
2021-09-21 15:06:15 +0200

    Removed the connect link for MongoDb for security reasons

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import Student from "./models/Student.js";
 //connection to mongoDB using mongoose
 
 const MONGO_URI =
-  "mongodb+srv://new_use_1:7qQHu6lDWhWj3Me9@cluster0.ft5sz.mongodb.net/students_db?retryWrites=true&w=majority";
+  "";
 
 mongoose
   .connect(MONGO_URI, {
